ec9c33a91b41c6a51bcf495c4b5bddbb656087ff
magelle <maxime.gelle@gmail.com>
2021-05-21 09:44:58 +0200

    Add contract testing

--- src/test/java/devoxx/university/cashregister/infrastructure/FruitPriceRepositoryIntegrationTest.java
+++ src/test/java/devoxx/university/cashregister/infrastructure/FruitPriceRepositoryIntegrationTest.java
@@ -4,22 +4,23 @@ import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
-import au.com.dius.pact.core.model.annotations.PactFolder;
+import au.com.dius.pact.core.model.annotations.PactDirectory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.io.IOException;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(PactConsumerTestExt.class)
-@PactFolder("target/pacts")
+@PactDirectory("target/pacts")
+@PactTestFor(providerName = "FruitsProvider")
 class FruitPriceRepositoryIntegrationTest {
 
-    @Pact(provider = "FruitsProvider", consumer = "CashRegister")
+    @Pact(consumer = "CashRegister")
     public RequestResponsePact createPact(PactDslWithProvider builder) {
         return builder
                 .uponReceiving("Get a single fruit, by name")
@@ -35,7 +36,7 @@ class FruitPriceRepositoryIntegrationTest {
     }
 
     @Test
-    void test(MockServer mockServer) throws IOException {
+    public void should_retrieve_a_fruit(MockServer mockServer) {
         FruitPriceRepository fruitPriceRepository = new FruitPriceRepository(mockServer.getUrl());
         long price = fruitPriceRepository.retrieveFruitPrice("Fraises");
         assertThat(price).isEqualTo(33);
--- src/test/java/devoxx/university/fruits/FruitControllerIntegrationTest.java
+++ src/test/java/devoxx/university/fruits/FruitControllerIntegrationTest.java
@@ -1,10 +1,12 @@
 package devoxx.university.fruits;
 
+import au.com.dius.pact.core.model.annotations.PactDirectory;
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import au.com.dius.pact.provider.junitsupport.loader.PactSource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
