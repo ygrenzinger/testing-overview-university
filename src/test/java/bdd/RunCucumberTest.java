0651ea84e596610224e508473eb72554f731c036
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2021-05-25 13:34:47 +0200

    Preparing BDD live coding

--- src/test/java/bdd/RunCucumberTest.java
+++ src/test/java/bdd/RunCucumberTest.java
@@ -3,6 +3,5 @@ package bdd;
 import io.cucumber.junit.platform.engine.Cucumber;
 
 @Cucumber
-//@CucumberOptions(plugin = {"pretty", "html:target/"}, features="src/test/resources/bdd")
 public class RunCucumberTest {
 }
